8549e083cb421e6e0fac4104f95d7d7df0d80460
Bhargavnaidu126 <bhargavnaidupola234@gmail.com>
2026-04-14 16:06:13 +0530

    Initial calculator project

---
+++ app.js
@@ -0,0 +1,12 @@
+// calculator-app/app.js - MODULE 1
+function calculate(num1, num2, operation) {
+ console.log(`Calculating ${num1} ${operation} ${num2}`);
+ 
+ if (operation === "add") {
+ return num1 + num2; // Implement this
+ }
+ 
+ return "Operation not implemented";
+}
+
+console.log(calculate(5, 3, "add"));
